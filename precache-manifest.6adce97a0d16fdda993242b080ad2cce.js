22ac73a27f301fdd4c967ae236b72eaab39d156d
Elizabeth Jimenez <izavetha@gmail.com>
2019-03-02 23:14:36 -0600

    Updates

---
+++ precache-manifest.6adce97a0d16fdda993242b080ad2cce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cerberus/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a8997e33a185f23b55e4",
+    "url": "/cerberus/static/js/runtime~main.a8997e33.js"
+  },
+  {
+    "revision": "b289ec5cef851755f872",
+    "url": "/cerberus/static/js/main.b289ec5c.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/cerberus/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "b289ec5cef851755f872",
+    "url": "/cerberus/static/css/main.a1ffdc26.chunk.css"
+  },
+  {
+    "revision": "b7d02ec868026d81bdce3e813f2e1ba1",
+    "url": "/cerberus/index.html"
+  }
+];
